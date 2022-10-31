c632628fb478390aa4f7a8e2067f63924b8ef1bb
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-10-31 11:43:47 +0600

    feat: add login css

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,17 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div style={{ paddingTop: "20px" }} className="loginForm">
+      <div className="formContainer">
+        <label>Username: </label>
+        <input type="text" />
+        <label>Password: </label>
+        <input type="password" />
+        <button>Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
