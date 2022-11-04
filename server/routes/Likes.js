0b4aa5881d1161825d80aa1046b466ae3a59fd4f
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-04 18:08:17 +0600

    feat: add like/unlike server logic

--- server/routes/Likes.js
+++ server/routes/Likes.js
@@ -7,8 +7,18 @@ router.post("/", validateToken, async (req, res) => {
   const { PostId } = req.body;
   const UserId = req.user.id;
 
-  await Likes.create({ PostId: PostId, UserId: UserId });
-  res.json("success");
+  const found = await Likes.findOne({
+    where: { PostId: PostId, UserId: UserId },
+  });
+  if (!found) {
+    await Likes.create({ PostId: PostId, UserId: UserId });
+    res.json("Liked The Post");
+  } else {
+    await Likes.destroy({
+      where: { PostId: PostId, UserId: UserId },
+    });
+    res.json("UnLiked The Post");
+  }
 });
 
 module.exports = router;
