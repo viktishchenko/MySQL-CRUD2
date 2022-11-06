ec8e97f6161e854f635d095efd47013f84f564a5
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-06 07:06:08 +0600

    feat: add profile route, get user info

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Profile = () => {
   const { id } = useParams(); // get id
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    axios.get(`http://localhost:8800/auth/userinfo/${id}`).then((res) => {
+      setUsername(res.data.username);
+    });
+  }, []);
 
   return (
     <div className="profilePagecontainer">
       <div className="basicInfo">
-        <h1>Username: {id}</h1>
+        <h1>Username: {username}</h1>
       </div>
       <div className="listOfPosts">posts</div>
     </div>
